de4e0e3b72afe834e992eba8052cf1f191f39f38
moni <126834154+mgarcia1306@users.noreply.github.com>
2026-07-14 00:52:20 -0400

    Update formulario.js
    html improvement

--- js/formulario.js
+++ js/formulario.js
@@ -58,7 +58,7 @@ function formatearRUT(rut) {
 // =========================================================================
 // 2. PROCESAMIENTO Y ENVÍO CENTRALIZADO
 // =========================================================================
-const URL_SHEETS = "https://script.google.com/macros/s/AKfycbxjPDWG5QoIopSCPYU5nju0JqCOlONm8UH_Al3v6HBJfZYcgYCwHjeMWb1u83FqFSBq/exec";
+const URL_SHEETS = "https://script.google.com/macros/s/AKfycbw8E7eFRYMdTlxouaoqF8_HlAWNs-VuUWYBWNbbOHg14umBTjNc_rR01pq-WFSv34Tu/exec";
 const form = document.getElementById("clienteForm");
 
 if (form) {
